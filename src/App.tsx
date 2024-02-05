d6cdfbb5c8b17d7376e6a4a473764f181977b59b
diogocorigo <diogoacorigo@gmail.com>
2024-02-05 22:31:03 +0000

    sidebar template done

--- src/App.tsx
+++ src/App.tsx
@@ -1,6 +1,14 @@
-import { Home, Library } from "lucide-react";
+import { Calendar, Home, Library, Star, Clock10 } from "lucide-react";
 import { Input } from "./components/ui/input";
 import { Separator } from "./components/ui/separator";
+import { Avatar, AvatarImage, AvatarFallback } from "./components/ui/avatar";
+import {
+    DropdownMenu,
+    DropdownMenuContent,
+    DropdownMenuItem,
+    DropdownMenuSeparator,
+    DropdownMenuTrigger,
+} from "@/components/ui/dropdown-menu";
 
 export default function App() {
     return (
@@ -16,6 +24,12 @@ export default function App() {
                         <Home /> <span>Home</span>
                     </a>
                     <a
+                        href="#calendar"
+                        className="flex gap-4 p-2 rounded hover:bg-zinc-700"
+                    >
+                        <Calendar /> <span>Calendar</span>
+                    </a>
+                    <a
                         href="#projects"
                         className="flex gap-4 p-2 rounded hover:bg-zinc-700"
                     >
@@ -23,10 +37,98 @@ export default function App() {
                     </a>
                 </div>
                 <Separator className="bg-zinc-800 my-4" />
-                {/* Projects Search Bar */}
+                {/* Projects */}
+                <div className="flex flex-1 flex-col">
+                    <Input
+                        placeholder="Search Projects"
+                        className="border-zinc-700 focus:border-zinc-100 border-2 py-5 text-zinc-100"
+                    />
+                    <h1 className="flex items-center gap-2 mt-4 mb-2 px-2 text-md">
+                        <Star className="w-5 h-5" /> Favorite Projects:
+                    </h1>
+                    <div className="flex flex-col gap-2 pl-4">
+                        <a
+                            href="#"
+                            className="text-sm text-zinc-400 hover:text-zinc-200"
+                        >
+                            Project XPTO
+                        </a>
+                        <a
+                            href="#"
+                            className="text-sm text-zinc-400 hover:text-zinc-200"
+                        >
+                            Project OTPX
+                        </a>
+                        <a
+                            href="#"
+                            className="text-sm text-zinc-400 hover:text-zinc-200"
+                        >
+                            Project PXTO
+                        </a>
+                    </div>
+                    <Separator className="bg-zinc-800 mt-4" />
+                    <h1 className="flex items-center gap-2 mt-4 mb-2 px-2 text-md">
+                        <Clock10 className="w-5 h-5" /> Recent Projects:
+                    </h1>
+                    <div className="flex flex-col gap-2 pl-4">
+                        <a
+                            href="#"
+                            className="text-sm text-zinc-400 hover:text-zinc-200"
+                        >
+                            Project XPTO
+                        </a>
+                        <a
+                            href="#"
+                            className="text-sm text-zinc-400 hover:text-zinc-200"
+                        >
+                            Project OTPX
+                        </a>
+                        <a
+                            href="#"
+                            className="text-sm text-zinc-400 hover:text-zinc-200"
+                        >
+                            Project PXTO
+                        </a>
+                        <a
+                            href="#"
+                            className="text-sm text-zinc-400 hover:text-zinc-200"
+                        >
+                            Project TXPO
+                        </a>
+                        <a
+                            href="#"
+                            className="text-sm text-zinc-400 hover:text-zinc-200"
+                        >
+                            Project XOPT
+                        </a>
+                    </div>
+                </div>
+                {/* Profile */}
+                <div className="p-2 rounded hover:bg-zinc-700 cursor-pointer">
+                    <DropdownMenu>
+                        <DropdownMenuTrigger className="w-full flex items-center gap-4">
+                            <Avatar>
+                                <AvatarImage src="https://github.com/shadcn.png" />
+                                <AvatarFallback>US</AvatarFallback>
+                            </Avatar>
+                            <h1 className="text-xl text-zinc-200">
+                                Utilizador
+                            </h1>
+                        </DropdownMenuTrigger>
+                        <DropdownMenuContent
+                            side="right"
+                            className="bg-zinc-900 border-zinc-800 text-zinc-100"
+                        >
+                            <DropdownMenuItem>Change Theme</DropdownMenuItem>
+                            <DropdownMenuItem>Change Language</DropdownMenuItem>
+                            <DropdownMenuSeparator className="bg-zinc-800" />
+                            <DropdownMenuItem>Accout</DropdownMenuItem>
+                        </DropdownMenuContent>
+                    </DropdownMenu>
+                </div>
             </aside>
             {/* Main */}
-            <main className="flex-1">
+            <main className="flex-1 p-4">
                 <h1>Main</h1>
             </main>
         </div>
