639acff2315d8da09453f2e74cb882f78b9ede8a
diogocorigo <diogoacorigo@gmail.com>
2024-02-07 19:58:34 +0000

    added scrollarea and datatables

--- src/main.tsx
+++ src/main.tsx
@@ -3,6 +3,7 @@ import ReactDOM from "react-dom/client";
 import { createBrowserRouter, RouterProvider } from "react-router-dom";
 import "./globals.css";
 import { Sidebar } from "@/components/custom/Sidebar";
+import { ScrollArea } from "@/components/ui/scroll-area";
 
 import ErrorPage from "./error-page";
 import Home from "./routes/home";
@@ -33,9 +34,9 @@ ReactDOM.createRoot(document.getElementById("root") as HTMLElement).render(
             {/* Sidebar */}
             <Sidebar />
             {/* Main */}
-            <main className="flex-1 p-4">
+            <ScrollArea className="flex-1 p-4 pb-0">
                 <RouterProvider router={router} />
-            </main>
+            </ScrollArea>
         </div>
     </React.StrictMode>
 );
--- src/routes/home.tsx
+++ src/routes/home.tsx
@@ -7,21 +7,14 @@ import {
     CardTitle,
 } from "@/components/ui/card";
 
+
 export default function Home() {
     return (
         <Card className="bg-zinc-800 border-zinc-700 text-zinc-100 rounded">
             <CardHeader>
                 <CardTitle className="text-4xl">Favorite Projects</CardTitle>
             </CardHeader>
-            <CardContent className="grid grid-cols-3 gap-4">
-                <Card className="p-4 rounded bg-zinc-500">
-                    <CardHeader>
-                        <CardTitle className="text-2xl">
-                            Project XPTO
-                        </CardTitle>
-                    </CardHeader>
-                </Card>
-            </CardContent>
+            <CardContent className="grid grid-cols-3 gap-4"></CardContent>
             <CardFooter className="justify-center">
                 <a href="/projects" className="text-sm">
                     View All
